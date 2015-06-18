c097d5ef6ba9b2ca2531d0dc340f7007ec0b6d0d
LeoBessa403 <leonardomcbessa@gmail.com>
2015-06-18 10:04:13 -0300

    Correções

--- library/Conn/Pesquisa.class.php
+++ library/Conn/Pesquisa.class.php
@@ -67,10 +67,9 @@ class Pesquisa extends Conn {
     }
     
     /**
-     * <b>Seta os dados:</b> Dados a serem substituidos na query de pesquisa.  
-     * @param STRING $Valores = variavel={$valor}&variavel2={$valor2}
+     * <b>getSql:</b> Retorna o SQL que esta sendo Executado.  
      */
-    public function getSelect() {
+    public function getSql() {
        return $this->Select;
     }
 
